a8615482435d20226ef689ab209339d7f77753f5
masoom70 <ajaykurmi702@gmail.com>
2026-01-03 01:56:00 +0530

    Update __init__.py
    Signed-off-by: masoom70 <ajaykurmi702@gmail.com>

--- AnonXMusic/plugins/__init__.py
+++ AnonXMusic/plugins/__init__.py
@@ -1,19 +1,15 @@
-import glob
-from os.path import dirname, isfile
+from pathlib import Path
 
 
-def __list_all_modules():
-    work_dir = dirname(__file__)
-    mod_paths = glob.glob(work_dir + "/*/*.py")
+def _list_all_modules():
+    work_dir = Path(__file__).parent
 
-    all_modules = [
-        (((f.replace(work_dir, "")).replace("/", "."))[:-3])
-        for f in mod_paths
-        if isfile(f) and f.endswith(".py") and not f.endswith("__init__.py")
-    ]
-
-    return all_modules
+    return sorted(
+        "." + ".".join(path.relative_to(work_dir).with_suffix("").parts)
+        for path in work_dir.glob("*/*.py")
+        if path.name != "__init__.py"
+    )
 
 
-ALL_MODULES = sorted(__list_all_modules())
+ALL_MODULES = _list_all_modules()
 __all__ = ALL_MODULES + ["ALL_MODULES"]
