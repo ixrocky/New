fc5534e4175a2b5f3be5dfb60bef8920303dab7d
masoom70 <ajaykurmi702@gmail.com>
2026-01-03 01:45:52 +0530

    Update __main__.py
    Signed-off-by: masoom70 <ajaykurmi702@gmail.com>

--- AnonXMusic/__main__.py
+++ AnonXMusic/__main__.py
@@ -33,9 +33,10 @@ async def init():
             BANNED_USERS.add(user_id)
     except:
         pass
-    await app.start()
+    await app._start()
     for all_module in ALL_MODULES:
         importlib.import_module("AnonXMusic.plugins" + all_module)
+    LOGGER("AnonXMusic").info(f"Loaded {len(ALL_MODULES)} Modules.")
     LOGGER("AnonXMusic.plugins").info("Successfully Imported Modules...")
     await userbot.start()
     await Anony.start()
@@ -43,7 +44,7 @@ async def init():
         await Anony.stream_call("https://te.legra.ph/file/29f784eb49d230ab62e9e.mp4")
     except NoActiveGroupCall:
         LOGGER("AnonXMusic").error(
-            "Please turn on the videochat of your log group\channel.\n\nStopping Bot..."
+            "Please turn on the videochat of your log group/channel.\n\nStopping Bot..."
         )
         exit()
     except:
@@ -53,7 +54,7 @@ async def init():
         "\x41\x6e\x6f\x6e\x58\x20\x4d\x75\x73\x69\x63\x20\x42\x6f\x74\x20\x53\x74\x61\x72\x74\x65\x64\x20\x53\x75\x63\x63\x65\x73\x73\x66\x75\x6c\x6c\x79\x2e\n\n\x44\x6f\x6e'\x74\x20\x66\x6f\x72\x67\x65\x74\x20\x74\x6f\x20\x76\x69\x73\x69\x74\x20\x40\x46\x61\x6c\x6c\x65\x6e\x41\x73\x73\x6f\x63\x69\x61\x74\x69\x6f\x6e"
     )
     await idle()
-    await app.stop()
+    await app._stop()
     await userbot.stop()
     LOGGER("AnonXMusic").info("Stopping AnonX Music Bot...")
 
